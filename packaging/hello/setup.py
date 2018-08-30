0fe5fc17f34eeb461807925645fea48f1445b0e9
athlonxpgzw <athlonxpgzw@gmail.com>
2018-08-30 15:18:05 +0800

    added packaging samples

---
+++ packaging/hello/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from setuptools import setup
+setup(name='Hello',
+    version='1.0',
+    description='A simple example',
+    author='Magnus Lie Hetland',
+    py_modules=['hello'])
