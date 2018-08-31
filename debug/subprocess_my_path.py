3d1cb1b865a527094adf2d0969690e39bd596bda
athlonxpgzw <athlonxpgzw@gmail.com>
2018-08-31 14:52:17 +0800

    added some files

---
+++ debug/subprocess_my_path.py
@@ -0,0 +1,16 @@
+import unittest, my_math
+from subprocess import Popen, PIPE
+
+class ProductTestCase(unittest.TestCase):
+
+    def test_with_PyChecker(self):
+        cmd = 'pychecker', '-Q', my_math.__file__.rstrip('c')
+        pychecker = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        self.assertEqual(pychecker.stdout.read(), '')
+
+    def test_with_PyLint(self):
+        cmd = 'pylint', '-rn', 'my_math'
+        pylint = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        self.assertEqual(pylint.stdout.read(), '')
+
+if __name__ == '__main__': unittest.main()
